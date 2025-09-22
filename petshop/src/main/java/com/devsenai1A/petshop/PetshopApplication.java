382ef09572b468807e4b4ef27deb58902c2049d9
GabrielaGrandini <gabriela.grandini1a@gmail.com>
2025-09-22 14:54:24 -0300

    Projeto com o HELLO WORLD

--- petshop/src/main/java/com/devsenai1A/petshop/PetshopApplication.java
+++ petshop/src/main/java/com/devsenai1A/petshop/PetshopApplication.java
@@ -2,6 +2,8 @@ package com.devsenai1A.petshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PetshopApplication {
@@ -9,5 +11,13 @@ public class PetshopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PetshopApplication.class, args);
 	}
-
+	@RestController
+	static class HelloWorldController {
+		
+		@GetMapping("/")
+		public String helloWorld() {
+			return "Hello World!";
+		}
+	}
 }
+
