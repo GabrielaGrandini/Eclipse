9458ad0ba91b80dedc74483eaac60c30bad2a465
GabrielaGrandini <gabriela.grandini1a@gmail.com>
2025-09-29 15:39:23 -0300

    calculadora

---
+++ calculadora/src/main/java/com/devsenai1A/calculadora/calculadoraApplication.java
@@ -0,0 +1,13 @@
+package com.devsenai1A.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class calculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(calculadoraApplication.class, args);
+	}
+
+}
